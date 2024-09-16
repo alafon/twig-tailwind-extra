d6932ada087c9bfbe336f877d15dfafda4e985b3
Arnaud Lafon <arnaud.lafon@gmail.com>
2024-09-16 14:44:14 +0200

    Do not use AbstractBundle if not needed

--- src/Bridge/Symfony/Bundle/TalesFromADevTwigExtraTailwindBundle.php
+++ src/Bridge/Symfony/Bundle/TalesFromADevTwigExtraTailwindBundle.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle;
 
 use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
 use TalesFromADev\Twig\Extra\Tailwind\Bridge\Symfony\Bundle\DependencyInjection\TalesFromADevTwigExtraTailwindExtension;
 
-final class TalesFromADevTwigExtraTailwindBundle extends AbstractBundle
+final class TalesFromADevTwigExtraTailwindBundle extends Bundle
 {
     public function getContainerExtension(): ?ExtensionInterface
     {
